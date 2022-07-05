3472c421067231489773c695fe69ac8ce8a22b7c
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-05 23:33:41 +0800

    Add filters for station
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- operation/schema/filters.py
+++ operation/schema/filters.py
@@ -1,5 +1,7 @@
 import strawberry
 import strawberry_django
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import Distance
 
 from operation import models
 from operation.schema.enums import AssetType
@@ -39,3 +41,32 @@ class AssetFilter:
 
     def filter_asset_type(self, queryset):
         return queryset.filter(asset_type=self.asset_type)
+
+
+@strawberry_django.filters.filter(models.Station)
+class StationFilter:
+    id: strawberry.ID
+    display_name: str
+    internal_representation: str
+    location: strawberry.scalars.JSON
+
+    def filter_id(self, queryset):
+        return queryset.filter(id=self.id)
+
+    def filter_display_name(self, queryset):
+        return queryset.filter(display_name__icontains=self.display_name)
+
+    def filter_internal_representation(self, queryset):
+        return queryset.filter(internal_representation=self.internal_representation)
+
+    def filter_location(self, queryset):
+        return queryset.filter(
+            location__distance_lt=(
+                Point(
+                    self.location.get("x"),
+                    self.location.get("y"),
+                    self.location.get("z"),
+                ),
+                Distance(km=self.location.get("radius")),
+            )
+        )
--- operation/schema/schema.py
+++ operation/schema/schema.py
@@ -3,7 +3,7 @@ import typing
 import strawberry
 import strawberry_django
 
-from operation.schema.filters import AssetFilter, LineFilter
+from operation.schema.filters import AssetFilter, LineFilter, StationFilter
 from operation.schema.scalars import Asset, Line, Station
 
 
@@ -11,4 +11,6 @@ from operation.schema.scalars import Asset, Line, Station
 class OperationScalars:
     lines: typing.List[Line] = strawberry_django.field(filters=LineFilter)
     assets: typing.List[Asset] = strawberry_django.field(filters=AssetFilter)
-    stations: typing.List[Station] = strawberry_django.field()
+    stations: typing.List[Station] = strawberry_django.field(
+        filters=StationFilter,
+    )
