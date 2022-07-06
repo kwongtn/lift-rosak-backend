5e355e34e11402b66f0bfcbc732bade96b378cf6
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-07 01:59:11 +0800

    Update reporting models & migrations
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- reporting/models.py
+++ reporting/models.py
@@ -7,19 +7,49 @@ from reporting.enums import ReportType
 
 
 class Report(TimeStampedModel, UUIDModel, SoftDeletableModel):
-    asset = models.ForeignKey(to="operation.Asset")
-    reporter = models.ForeignKey(to="common.User")
+    asset = models.ForeignKey(
+        to="operation.Asset",
+        on_delete=models.PROTECT,
+    )
+    reporter = models.ForeignKey(
+        to="common.User",
+        on_delete=models.PROTECT,
+    )
     description = models.TextField(default="")
-    type = models.TextField(choices=ReportType)
+    type = models.TextField(choices=ReportType.choices)
 
 
 class Resolution(TimeStampedModel, UUIDModel, SoftDeletableModel):
-    reports = models.ManyToManyField()
+    reports = models.ManyToManyField(
+        to="reporting.Report",
+        through="reporting.ReportResolution",
+    )
+    user = models.ForeignKey(
+        to="common.User",
+        on_delete=models.PROTECT,
+    )
 
 
-class ReportVote(TimeStampedModel):
-    report = models.ForeignKey(to="reporting.Report")
-    user = models.ForeignKey(to="common.User")
+class ReportResolution(models.Model):
+    resolution = models.ForeignKey(
+        to="reporting.Resolution",
+        on_delete=models.PROTECT,
+    )
+    report = models.ForeignKey(
+        to="reporting.Report",
+        on_delete=models.PROTECT,
+    )
+
+
+class Vote(TimeStampedModel):
+    report = models.ForeignKey(
+        to="reporting.Report",
+        on_delete=models.PROTECT,
+    )
+    user = models.ForeignKey(
+        to="common.User",
+        on_delete=models.PROTECT,
+    )
     is_upvote = models.BooleanField()
 
     @property
