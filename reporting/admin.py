5c6629dfc36754304bbe4fdbe44abf214f81c07e
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-07 01:58:58 +0800

    Update reporting admin
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- reporting/admin.py
+++ reporting/admin.py
@@ -1,3 +1,38 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from reporting.models import Report, ReportResolution, Resolution, Vote
+
+
+class ReportStackedInline(admin.StackedInline):
+    model = Report
+
+
+class ResolutionStackedInline(admin.StackedInline):
+    model = Resolution
+
+
+class VoteStackedInline(admin.StackedInline):
+    model = Vote
+
+
+class ReportResolutionStackedInline(admin.StackedInline):
+    model = ReportResolution
+
+
+class ReportAdmin(admin.ModelAdmin):
+    pass
+
+
+class ResolutionAdmin(admin.ModelAdmin):
+    inlines = [
+        ReportResolutionStackedInline,
+    ]
+
+
+class VoteAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Report, ReportAdmin)
+admin.site.register(Resolution, ResolutionAdmin)
+admin.site.register(Vote, VoteAdmin)
