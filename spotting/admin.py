58dd9946f8aea729093dffd76dd417810df86b10
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-10 22:35:03 +0800

    Enhance spotting admin
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- spotting/admin.py
+++ spotting/admin.py
@@ -13,6 +13,24 @@ class EventForm(GeometricForm):
 
 class EventAdmin(admin.ModelAdmin):
     form = EventForm
+    list_display = [
+        "id",
+        "reporter",
+        "vehicle",
+        "status",
+        "type",
+        "origin_station",
+        "destination_station",
+        "location",
+    ]
+    list_filter = [
+        "type",
+        "vehicle",
+        "reporter",
+        "status",
+        "origin_station",
+        "destination_station",
+    ]
 
 
 admin.site.register(Event, EventAdmin)
