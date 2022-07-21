4185c332a6d43458f487596c13f4abef87a55671
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-22 00:26:54 +0800

    Fix debug env incorrect
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- rosak/settings.py
+++ rosak/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from distutils.util import strtobool
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = strtobool(os.getenv("DEBUG", "false"))
 
 # LOGGING = {
 #     "version": 1,
