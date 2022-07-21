a0c3687b2e1efe3349f7226d21dd88fff2853110
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-21 20:17:42 +0800

    Optimize & fix devops related
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- run_app.sh
+++ run_app.sh
@@ -8,5 +8,5 @@ if [ "$DEBUG" == 'True' ]; then
     pipenv run python manage.py check # Doing it manually since checks aren't run by WSGI stack
     pipenv run gunicorn rosak.asgi:application -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001 --reload --config gunicorn.dev.conf.py
 else
-    pipenv run gunicorn rosak.asgi:application -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001
+    pipenv run gunicorn rosak.asgi:application -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001
 fi
