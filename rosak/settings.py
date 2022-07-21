b67db6616648a3327610acaaac4278ad1af39ce9
Kwong Tung Nan <tungnan5636@gmail.com>
2022-07-22 01:18:44 +0800

    Fix array issue
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- rosak/settings.py
+++ rosak/settings.py
@@ -95,7 +95,8 @@ MIDDLEWARE = [
 
 CSRF_TRUSTED_ORIGINS = [
     "http://kwongnet.ddns.net",
-    "http://lift-rosak.ddns.net" "http://127.0.0.1",
+    "https://lift-rosak.ddns.net:8000",
+    "http://127.0.0.1",
     "http://localhost",
 ]
 
