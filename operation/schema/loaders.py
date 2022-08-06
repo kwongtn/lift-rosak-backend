f0eb3d9043e14e3d04516c93083727479d81a2af
Kwong Tung Nan <tungnan5636@gmail.com>
2022-08-06 15:56:45 +0800

    Update loaders to use new async for at Django 4.1
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- operation/schema/loaders.py
+++ operation/schema/loaders.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 from typing import List
 
-from asgiref.sync import sync_to_async
 from django.db.models import Count, Max, Q
 from strawberry.dataloader import DataLoader
 
@@ -9,10 +8,9 @@ from operation.models import Vehicle
 from spotting.models import Event
 
 
-@sync_to_async
-def batch_load_vehicle_from_vehicle_type(keys):
+async def batch_load_vehicle_from_vehicle_type(keys):
     vehicle_type_dict = defaultdict()
-    for vehicle in Vehicle.objects.filter(vehicle_type_id__in=keys).iterator():
+    async for vehicle in Vehicle.objects.filter(vehicle_type_id__in=keys):
         if vehicle.vehicle_type_id not in vehicle_type_dict:
             vehicle_type_dict[vehicle.vehicle_type_id] = {vehicle}
         else:
@@ -21,14 +19,13 @@ def batch_load_vehicle_from_vehicle_type(keys):
     return [list(vehicle_type_dict.get(key, {})) for key in keys]
 
 
-@sync_to_async
-def batch_load_vehicle_type_from_line(keys):
+async def batch_load_vehicle_type_from_line(keys):
     vehicles: List[Vehicle] = Vehicle.objects.filter(line_id__in=keys).prefetch_related(
         "vehicle_type"
     )
 
     line_dict = defaultdict()
-    for vehicle in vehicles:
+    async for vehicle in vehicles:
         if vehicle.line_id not in line_dict:
             line_dict[vehicle.line_id] = {vehicle.vehicle_type}
         else:
@@ -37,10 +34,9 @@ def batch_load_vehicle_type_from_line(keys):
     return [list(line_dict.get(key, {})) for key in keys]
 
 
-@sync_to_async
-def batch_load_vehicle_status_count_from_vehicle_type(keys):
+async def batch_load_vehicle_status_count_from_vehicle_type(keys):
     # Keys in format (vehicle_type_id, status)
-    vehicle_object = Vehicle.objects.aggregate(
+    vehicle_object = await Vehicle.objects.aaggregate(
         **{
             str(key[0])
             + str(key[1]): Count("id", filter=Q(vehicle_type_id=key[0], status=key[1]))
@@ -51,28 +47,25 @@ def batch_load_vehicle_status_count_from_vehicle_type(keys):
     return [vehicle_object.get(str(key[0]) + str(key[1]), None) for key in keys]
 
 
-@sync_to_async
-def batch_load_vehicle_count_from_vehicle_type(keys):
-    vehicle_object = Vehicle.objects.aggregate(
+async def batch_load_vehicle_count_from_vehicle_type(keys):
+    vehicle_object = await Vehicle.objects.aaggregate(
         **{str(key): Count("id", filter=Q(vehicle_type_id=key)) for key in keys}
     )
 
     return [vehicle_object.get(str(key), None) for key in keys]
 
 
-@sync_to_async
-def batch_load_last_spotting_date_from_vehicle_id(keys):
-    event_object = Event.objects.aggregate(
+async def batch_load_last_spotting_date_from_vehicle_id(keys):
+    event_object = await Event.objects.aaggregate(
         **{str(key): Max("spotting_date", filter=Q(vehicle_id=key)) for key in keys}
     )
 
     return [event_object.get(str(key), None) for key in keys]
 
 
-@sync_to_async
-def batch_load_spotting_count_from_vehicle(keys):
+async def batch_load_spotting_count_from_vehicle(keys):
     # Keys in format (vehicle_id, filter)
-    event_object = Event.objects.aggregate(
+    event_object = await Event.objects.aaggregate(
         **{str(key[0]): Count("id", filter=key[1]) for key in keys}
     )
 
