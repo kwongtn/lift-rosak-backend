e651967f9bd3bd39110eabe94e140a174b1587e1
Kwong Tung Nan <tungnan5636@gmail.com>
2022-08-30 18:20:03 +0800

    Initialize incident app
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

---
+++ incident/schema/schema.py
@@ -0,0 +1,12 @@
+from strawberry_django_plus import gql
+
+
+@gql.type
+class IncidentScalars:
+    # incidents
+    pass
+
+
+@gql.type
+class IncidentMutations:
+    pass
--- rosak/schema.py
+++ rosak/schema.py
@@ -1,7 +1,9 @@
+from strawberry.types import Info
 from strawberry_django_plus import gql
 from strawberry_django_plus.optimizer import DjangoOptimizerExtension
 
 from common.schema.schema import CommonMutations, CommonScalars
+from incident.schema.schema import IncidentMutations, IncidentScalars
 from operation.schema.schema import OperationMutations, OperationScalars
 from reporting.schema.schema import ReportingMutations, ReportingScalars
 from spotting.schema.schema import SpottingMutations, SpottingScalars
@@ -13,10 +15,15 @@ class Query(
     ReportingScalars,
     CommonScalars,
     SpottingScalars,
+    IncidentScalars,
 ):
     @gql.field
-    def hello(self) -> str:
+    def hello(self, info: Info) -> str:
         return "Hello World"
+
+    @gql.field
+    def hello2(self, info: Info) -> str:
+        return "Hello World 2"
 
 
 @gql.type
@@ -25,6 +32,7 @@ class Mutation(
     ReportingMutations,
     CommonMutations,
     SpottingMutations,
+    IncidentMutations,
 ):
     pass
 
