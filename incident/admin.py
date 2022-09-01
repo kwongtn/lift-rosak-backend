cf1ff957abbf433e5950da580448f616d4bb7a22
Kwong Tung Nan <tungnan5636@gmail.com>
2022-09-01 10:03:09 +0800

    Enhance admin for ordered models
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- incident/admin.py
+++ incident/admin.py
@@ -1,5 +1,5 @@
 from django.contrib.gis import admin, forms
-from ordered_model.admin import OrderedModelAdmin
+from ordered_model.admin import OrderedModelAdmin, OrderedTabularInline
 
 from generic.views import GeometricForm
 from incident.models import StationIncident, VehicleIncident
@@ -28,6 +28,15 @@ class VehicleIncidentAdmin(OrderedModelAdmin):
     list_filter = incident_list_filter + ["vehicle__lines"]
 
 
+class VehicleIncidentInlineAdmin(OrderedTabularInline):
+    form = VehicleIncidentLocationForm
+    model = VehicleIncident
+    classes = ["collapse"]
+    readonly_fields = ("move_up_down_links", "order")
+    ordering = ("order",)
+    extra = 1
+
+
 class StationIncidentLocationForm(GeometricForm):
     field_name = "location"
     required = False
@@ -39,6 +48,15 @@ class StationIncidentAdmin(OrderedModelAdmin):
     form = StationIncidentLocationForm
     list_display = incident_list_display + ["station"]
     list_filter = incident_list_filter + ["station__lines"]
+
+
+class StationIncidentInlineAdmin(OrderedTabularInline):
+    form = StationIncidentLocationForm
+    model = StationIncident
+    classes = ["collapse"]
+    readonly_fields = ("move_up_down_links", "order")
+    ordering = ("order",)
+    extra = 1
 
 
 admin.site.register(VehicleIncident, VehicleIncidentAdmin)
