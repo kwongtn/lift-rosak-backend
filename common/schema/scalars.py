e6014de7274f8af9090f9484f5f96e4d9b2c7f0d
Kwong Tung Nan <tungnan5636@gmail.com>
2022-11-18 10:25:57 +0800

    Add spottings field to user & related dataloaders
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- common/schema/scalars.py
+++ common/schema/scalars.py
@@ -1,3 +1,6 @@
+from typing import TYPE_CHECKING, Annotated, List
+
+from strawberry.types import Info
 from strawberry_django_plus import gql
 
 from common import models
@@ -11,8 +14,19 @@ class Media:
 
 @gql.django.type(models.User)
 class User:
+    if TYPE_CHECKING:
+        from spotting.schema.scalars import EventScalar
+
     id: gql.auto
     firebase_id: str
+
+    @gql.django.field
+    async def spottings(
+        self, info: Info, count: int = 1
+    ) -> List[Annotated["EventScalar", gql.lazy("spotting.schema.scalars")]]:
+        return await info.context.loaders["common"]["spottings_from_user_loader"].load(
+            self.id
+        )
 
 
 @gql.type
