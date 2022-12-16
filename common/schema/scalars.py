b3e013a0c8fcf882851b5d380ff6416e2faac0d3
Kwong Tung Nan <tungnan5636@gmail.com>
2022-12-16 11:05:25 +0800

    fix(common): UserSpottingTrends to put zero value for no values in db
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- common/schema/scalars.py
+++ common/schema/scalars.py
@@ -1,12 +1,14 @@
 from datetime import date, timedelta
 from typing import TYPE_CHECKING, Annotated, List, Optional
 
+import pendulum
 from django.db.models import Count
 from strawberry.types import Info
 from strawberry_django_plus import gql
 
 from common import models
 from common.schema.types import UserSpottingTrend
+from common.utils import date_splitter
 from generic.schema.enums import DateGroupings
 from spotting import models as spotting_models
 
@@ -37,7 +39,7 @@ class UserScalar:
         return spotting_models.Event.objects.filter(reporter_id=self.id).acount()
 
     @gql.django.field
-    async def spotting_trends(
+    def spotting_trends(
         self,
         info: Info,
         start: Optional[date] = gql.UNSET,
@@ -50,16 +52,76 @@ class UserScalar:
             end = date.today()
 
         group_strs = ["spotting_date"]
+        range_type = "days"
         if group == DateGroupings.YEAR:
             group_strs = ["spotting_date__year"]
+            range_type = "years"
         elif group == DateGroupings.MONTH:
             group_strs = ["spotting_date__year", "spotting_date__month"]
+            range_type = "months"
         elif group == DateGroupings.DAY:
             group_strs = [
                 "spotting_date__year",
                 "spotting_date__month",
                 "spotting_date__day",
             ]
+
+        results = list(
+            spotting_models.Event.objects.filter(
+                reporter_id=self.id,
+                spotting_date__lte=end,
+                spotting_date__gte=start,
+            )
+            .values(*group_strs)
+            .annotate(count=Count("id"))
+            .values(*group_strs, "count")
+            .order_by(*[f"-{group_str}" for group_str in group_strs])
+        )
+
+        period = pendulum.period(start, end)
+        range = period.range(range_type)
+
+        if group in [DateGroupings.DAY, gql.UNSET]:
+            result_days = [
+                date_splitter(result["spotting_date"], range_type) for result in results
+            ]
+            for elem in range:
+                if date_splitter(elem, range_type) not in result_days:
+                    results.append(
+                        {
+                            "spotting_date": date(elem.year, elem.month, elem.day),
+                            "count": 0,
+                        }
+                    )
+            results = sorted(results, key=lambda d: d["spotting_date"])
+
+        elif group == DateGroupings.MONTH:
+            result_months = [
+                [result["spotting_date__year"], result["spotting_date__month"]]
+                for result in results
+            ]
+            for elem in range:
+                if [elem.year, elem.month] not in result_months:
+                    results.append(
+                        {
+                            "spotting_date__year": elem.year,
+                            "spotting_date__month": elem.month,
+                            "count": 0,
+                        }
+                    )
+
+            results = sorted(
+                results,
+                key=lambda d: f'{d["spotting_date__year"]}{d["spotting_date__month"]:02}',
+            )
+
+        elif group == DateGroupings.YEAR:
+            result_years = [result["spotting_date__year"] for result in results]
+            for elem in range:
+                if elem.year not in result_years:
+                    results.append({"spotting_date__year": elem.year, "count": 0})
+
+            results = sorted(results, key=lambda d: d["spotting_date__year"])
 
         return [
             UserSpottingTrend(
@@ -78,15 +140,7 @@ class UserScalar:
                 if value.get("spotting_date", None) is not None
                 else value.get("spotting_date__day", None),
             )
-            async for value in spotting_models.Event.objects.filter(
-                reporter_id=self.id,
-                spotting_date__lte=end,
-                spotting_date__gte=start,
-            )
-            .values(*group_strs)
-            .annotate(count=Count("id"))
-            .values(*group_strs, "count")
-            .order_by(*[f"-{group_str}" for group_str in group_strs])
+            for value in results
         ]
 
 
--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,6 @@
 import dataclasses
+from datetime import date
+from typing import List
 
 from asgiref.sync import sync_to_async
 from django.http import HttpRequest
@@ -26,3 +28,14 @@ class FirebaseUser:
         )[0]
 
         return user
+
+
+def date_splitter(input_date: date, type: str = "days") -> List[int]:
+    return_list = []
+    if type in ["years", "months", "days"]:
+        return_list.append(input_date.year)
+        if type in ["months", "days"]:
+            return_list.append(input_date.month)
+            if type == "days":
+                return_list.append(input_date.day)
+    return return_list
