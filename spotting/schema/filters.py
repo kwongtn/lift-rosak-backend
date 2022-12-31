5deb4696cf9bdfa9856ac18a80aac0cd67d0fc6d
Kwong Tung Nan <tungnan5636@gmail.com>
2022-12-31 20:04:10 +0800

    feat(spotting): Add new filters & removed deprecated ones
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- spotting/schema/filters.py
+++ spotting/schema/filters.py
@@ -1,4 +1,5 @@
-from datetime import date, timedelta
+from datetime import date, datetime
+from typing import List
 
 from django.db.models import F, Q, Subquery
 from strawberry.types import Info
@@ -11,13 +12,19 @@ from spotting import models
 class EventFilter:
     id: gql.auto
     vehicle_id: gql.ID
-    days_before: int
+    type_in: List[str]
+    created_start_datetime: datetime
+    created_end_datetime: datetime
     has_notes: bool
-    last_n: int
+    notes_contain: str
     different_status_than_vehicle: bool
+    status_in: List[str]
+    spotted_start_date: date
+    spotted_end_date: date
     is_read: bool
     only_mine: bool
     is_anonymous: bool
+    free_search: str
 
     def filter_id(self, queryset):
         return queryset.filter(id=self.id)
@@ -25,10 +32,36 @@ class EventFilter:
     def filter_vehicle_id(self, queryset):
         return queryset.filter(vehicle_id=self.vehicle_id)
 
-    def filter_days_before(self, queryset):
-        return queryset.filter(
-            created__gt=date.today() - timedelta(days=self.days_before)
-        )
+    def filter_type_in(self, queryset):
+        return queryset.filter(type__in=self.type_in)
+
+    def filter_created_start_datetime(self, queryset):
+        date_target = self.created_start_datetime
+        if self.created_end_datetime:
+            date_target = min([self.created_start_datetime, self.created_end_datetime])
+
+        return queryset.filter(created__gte=date_target)
+
+    def filter_created_end_datetime(self, queryset):
+        date_target = self.created_end_datetime
+        if self.created_start_datetime:
+            date_target = max([self.created_start_datetime, self.created_end_datetime])
+
+        return queryset.filter(created__lte=date_target)
+
+    def filter_spotted_start_date(self, queryset):
+        date_target = self.spotted_start_date
+        if self.spotted_end_date:
+            date_target = min([self.spotted_start_date, self.spotted_end_date])
+
+        return queryset.filter(spotting_date__gte=date_target)
+
+    def filter_spotted_end_date(self, queryset):
+        date_target = self.spotted_end_date
+        if self.spotted_start_date:
+            date_target = max([self.spotted_start_date, self.spotted_end_date])
+
+        return queryset.filter(spotting_date__lte=date_target)
 
     def filter_is_anonymous(self, queryset):
         return queryset.filter(is_anonymous=self.is_anonymous)
@@ -37,11 +70,11 @@ class EventFilter:
         filter = ~Q(notes="") if self.has_notes else Q(notes="")
         return queryset.filter(filter)
 
-    def filter_last_n(self, queryset):
-        return queryset.order_by("-created")[: self.last_n]
-
     def filter_different_status_than_vehicle(self, queryset):
         return queryset.filter(~Q(vehicle__status=F("status")))
+
+    def filter_status_in(self, queryset):
+        return queryset.filter(status__in=self.status_in)
 
     def filter_is_read(self, queryset, info: Info):
         if not info.context.user:
@@ -61,3 +94,12 @@ class EventFilter:
             return queryset.none()
 
         return queryset.filter(reporter_id=info.context.user.id)
+
+    def filter_free_search(self, queryset):
+        return queryset.filter(
+            Q(notes__icontains=self.free_search)
+            | Q(origin_station__display_name__icontains=self.free_search)
+            | Q(destination_station__display_name__icontains=self.free_search)
+            | Q(vehicle__identification_no__icontains=self.free_search)
+            | Q(vehicle__nickname__icontains=self.free_search)
+        )
