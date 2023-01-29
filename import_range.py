c6bcb046d2c941c75b9ecb19c4974b99535c426e
Kwong Tung Nan <tungnan5636@gmail.com>
2023-01-29 22:19:28 +0800

    feat(jejak): Reenable other imports for ranges
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- import_range.py
+++ import_range.py
@@ -1,7 +1,10 @@
+import argparse
 import os
 
 import django
 import pandas as pd
+
+from utils.constants import COL_RENAME, DTYPE, EXPECTED_COLS
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rosak.settings")
 django.setup()
@@ -19,7 +22,6 @@ from jejak.models import (  # noqa E402
     CaptainProviderRange,
     EngineStatus,
     EngineStatusBusRange,
-    Location,
     Provider,
     Route,
     Trip,
@@ -34,126 +36,93 @@ from utils.import_range_utils import (  # noqa E402
     single_side_multi_fk_range_import,
 )
 
-INPUT_FILENAME = "./utils/2022-05_0.json"
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-f", "--file", help="Input filename.")
 
-DT_TARGET = "dt_received"
+args = argParser.parse_args()
+# INPUT_FILENAME = "./utils/2022-05_0.json"
+INPUT_FILENAME: str = args.file
+FILENAME = INPUT_FILENAME.split("/")[-1]
 
-print("⏩ Reading data...")
+sleep_time = 5
+DT_TARGET = "dt_gps"
+
+print(f"{FILENAME} ⏩ Reading data...")
 df = pd.read_json(
     INPUT_FILENAME,
     lines=True,
-    dtype={
-        "latitude": "Float64",
-        "longitude": "Float64",
-        "dir": "Float32",
-        "speed": "Float32",
-        "angle": "Float32",
-        "captain_id": "Int32",
-        "trip_rev_kind": "Int32",
-        "engine_status": "Int32",
-        "accessibility": "Int32",
-        "busstop_id": "Int32",
+    dtype=DTYPE,
+).rename(columns=COL_RENAME)
+
+print(
+    f"{FILENAME} Not-expected columns: ",
+    EXPECTED_COLS.difference(set(df.columns.tolist())),
+)
+assert set(df.columns.tolist()) == EXPECTED_COLS
+
+for model in [Bus, Captain, TripRev, EngineStatus, Accessibility, Provider, BusStop]:
+    model_name = model.__name__.lower()
+    identifiers = list(df[model_name].dropna().unique())
+
+    print(f"⏩ Importing data for {model.__name__.lower()}...")
+    model.objects.bulk_create(
+        [model(identifier=identifier) for identifier in identifiers],
+        ignore_conflicts=True,
+    )
+
+for (range_model, left_model, right_model) in [
+    (BusProviderRange, Bus, Provider),
+    (AccessibilityBusRange, Bus, Accessibility),
+    (EngineStatusBusRange, Bus, EngineStatus),
+    (TripRevBusRange, Bus, TripRev),
+    (BusStopBusRange, Bus, BusStop),
+    (CaptainProviderRange, Captain, Provider),
+    (CaptainBusRange, Captain, Bus),
+]:
+    print(
+        f"⏩ Importing ranges for [{left_model.__name__.lower()}, {right_model.__name__.lower()}]..."
+    )
+    single_fk_range_import(
+        df=df,
+        range_model=range_model,
+        left_model=left_model,
+        right_model=right_model,
+    )
+
+multi_fk_row_import(
+    df=df,
+    groupings={
+        "provider": Provider,
+        "bus": Bus,
     },
-).rename(
-    columns={
-        "bus_no": "bus",
-        "trip_no": "trip",
-        "trip_rev_kind": "triprev",
-        "busstop_id": "busstop",
-        "captain_id": "captain",
-        "engine_status": "enginestatus",
-    }
+    target_str="trip",
+    target_model=Trip,
+)
+multi_fk_row_import(
+    df=df,
+    groupings={
+        "provider": Provider,
+    },
+    target_str="route",
+    target_model=Route,
 )
 
-# df['dir'] = (df['dir'] == 1990).astype(str)
 
-expected_cols = [
-    "dt_received",
-    "dt_gps",
-    "latitude",
-    "longitude",
-    "dir",
-    "speed",
-    "angle",
-    "route",
-    "bus",
-    "trip",
-    "captain",
-    "triprev",
-    "enginestatus",
-    "accessibility",
-    "provider",
-    "busstop",
-]
+single_side_multi_fk_range_import(
+    df=df,
+    range_model=TripRange,
+    side_model=Trip,
+    groupings={
+        "provider": Provider,
+        "bus": Bus,
+    },
+)
 
-print("Df columns: ", df.columns.tolist())
-print("Expected columns: ", expected_cols)
-
-assert df.columns.tolist() == expected_cols
-
-# for model in [Bus, Captain, TripRev, EngineStatus, Accessibility, Provider, BusStop]:
-#     print(f"⏩ Importing data for {model.__name__.lower()}...")
-#     model_name = model.__name__.lower()
-#     identifiers = list(df[model_name].dropna().unique())
-
-#     model.objects.abulk_create(
-#         [model(identifier=identifier) for identifier in identifiers],
-#         ignore_conflicts=True,
-#     )
-
-# for (range_model, left_model, right_model) in [
-#     (BusProviderRange, Bus, Provider),
-#     (AccessibilityBusRange, Bus, Accessibility),
-#     (EngineStatusBusRange, Bus, EngineStatus),
-#     (TripRevBusRange, Bus, TripRev),
-#     (BusStopBusRange, Bus, BusStop),
-#     (CaptainProviderRange, Captain, Provider),
-#     (CaptainBusRange, Captain, Bus),
-# ]:
-#     print(
-#         f"⏩ Importing ranges for [{left_model.__name__.lower()}, {right_model.__name__.lower()}]..."
-#     )
-#     single_fk_range_import(
-#         df=df,
-#         range_model=range_model,
-#         left_model=left_model,
-#         right_model=right_model,
-#     )
-
-# multi_fk_row_import(
-#     df=df,
-#     groupings={
-#         "provider": Provider,
-#         "bus": Bus,
-#     },
-#     target_str="trip",
-#     target_model=Trip,
-# )
-# multi_fk_row_import(
-#     df=df,
-#     groupings={
-#         "provider": Provider,
-#     },
-#     target_str="route",
-#     target_model=Route,
-# )
-
-
-# single_side_multi_fk_range_import(
-#     df=df,
-#     range_model=TripRange,
-#     side_model=Trip,
-#     groupings={
-#         "provider": Provider,
-#         "bus": Bus,
-#     },
-# )
-
-# multi_fk_range_import(
-#     df=df,
-#     range_model=BusRouteRange,
-#     left_model=Bus,
-#     right_model=Route,
-#     left_groupings=[],
-#     right_groupings=[Provider],
-# )
+multi_fk_range_import(
+    df=df,
+    range_model=BusRouteRange,
+    left_model=Bus,
+    right_model=Route,
+    left_groupings=[],
+    right_groupings=[Provider],
+)
