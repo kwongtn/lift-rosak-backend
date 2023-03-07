db3c947ea59489653b6e9a5fac0af41c61d4f07b
Kwong Tung Nan <tungnan5636@gmail.com>
2023-03-07 20:33:19 +0800

    feat(jejak): Add locations_count resolver
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- jejak/schema/schema.py
+++ jejak/schema/schema.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import List, Optional
 
 from strawberry_django_plus import gql
 
+from jejak.models import Location as LocationModel
 from jejak.schema.filters import LocationFilter
 from jejak.schema.orderings import BusOrder, LocationOrder
 from jejak.schema.scalars import Bus, Location
@@ -18,6 +19,26 @@ class JejakScalars:
         pagination=True,
         order=BusOrder,
     )
+
+    @gql.field
+    async def locations_count(self, filters: Optional[LocationFilter] = None) -> int:
+        query_dict = {
+            "bus_id": filters.bus_id,
+        }
+
+        if filters.dt_received_range:
+            query_dict["dt_received__range"] = (
+                min(filters.dt_received_range),
+                max(filters.dt_received_range),
+            )
+
+        if filters.dt_gps_range:
+            query_dict["dt_gps__range"] = (
+                min(filters.dt_gps_range),
+                max(filters.dt_gps_range),
+            )
+
+        return await LocationModel.objects.filter(**query_dict).acount()
 
 
 @gql.type
