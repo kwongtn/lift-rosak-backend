161f2026babf094218b4ff90ec96efd11bcf03fe
Kwong Tung Nan <tungnan5636@gmail.com>
2023-03-30 20:39:04 +0800

    feat(general): Updae dev secrets & command
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- run_app.sh
+++ run_app.sh
@@ -9,7 +9,7 @@ export GIT_COMMIT_TIME=$(date -r .git/refs/heads/main -R)
 
 if [ "$DEBUG" == 'True' ]; then
     pipenv run python manage.py check # Doing it manually since checks aren't run by WSGI stack
-    pipenv run gunicorn rosak.asgi:application -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001 --reload --config gunicorn.dev.conf.py
+    pipenv run gunicorn rosak.asgi:application -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001 --reload --config gunicorn.dev.conf.py
 else
     pipenv run gunicorn rosak.asgi:application -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001
 fi
