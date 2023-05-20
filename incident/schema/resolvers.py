e3b7e7ed60d30e3113298d6afd86b0f4db1badc3
Kwong Tung Nan <tungnan5636@gmail.com>
2023-05-20 10:51:56 +0800

    feat(insiden): Add count by month gql query
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- incident/schema/resolvers.py
+++ incident/schema/resolvers.py
@@ -25,9 +25,7 @@ async def get_calendar_incidents_by_severity_count(
 ) -> List[CalendarIncidentGroupByDateSeverityScalar]:
     assert start_date != strawberry.UNSET and end_date != strawberry.UNSET
 
-    if group_by == GroupByEnum.MONTH:
-        raise NotImplementedError()
-
+    return_list = []
     qs = CalendarIncident.objects.filter(
         Q(
             Q(start_datetime__date__lte=end_date)
@@ -41,7 +39,6 @@ async def get_calendar_incidents_by_severity_count(
             )
         )
     )
-
     min = (await qs.aaggregate(min=Min("start_datetime__date")))["min"]
     today = date.today()
 
@@ -52,35 +49,77 @@ async def get_calendar_incidents_by_severity_count(
         min if start_date > min else start_date,
         today if today < end_date else end_date,
     )
-    range = period.range("days")
 
     aggregations = {}
-    for range_date in range:
-        for severity in CalendarIncidentSeverity.values:
-            aggregations[f"{range_date}_{severity}"] = Count(
-                "id",
-                filter=Q(
-                    Q(start_datetime__date__lte=range_date)
-                    & Q(
-                        Q(end_datetime__date__gte=range_date)
-                        | Q(end_datetime__isnull=True)
-                    )
-                    & Q(severity=severity)
-                ),
-            )
+    if group_by == GroupByEnum.MONTH:
+        range = period.range("months")
+        for range_date in range:
+            range_str = range_date.strftime("%Y-%m")
+            range_year, range_month = range_str.split("-")
 
-    return_list = []
-    for key, value in (await qs.aaggregate(**aggregations)).items():
-        if value > 0:
-            incident_date, severity = key.split("_")
-            year, month, day = incident_date.split("-")
-
-            return_list.append(
-                CalendarIncidentGroupByDateSeverityScalar(
-                    date=date(int(year), int(month), int(day)),
-                    severity=severity,
-                    count=value,
+            for severity in CalendarIncidentSeverity.values:
+                aggregations[f"{range_str}_{severity}"] = Count(
+                    "id",
+                    filter=Q(
+                        Q(
+                            start_datetime__year__lte=range_year,
+                            start_datetime__month__lte=range_month,
+                        )
+                        & Q(
+                            Q(
+                                end_datetime__year__lte=range_year,
+                                end_datetime__month__lte=range_month,
+                            )
+                            | Q(end_datetime__isnull=True)
+                        )
+                        & Q(severity=severity)
+                    ),
                 )
-            )
+
+        for key, value in (await qs.aaggregate(**aggregations)).items():
+            if value > 0:
+                incident_date, severity = key.split("_")
+                year, month = incident_date.split("-")
+
+                return_list.append(
+                    CalendarIncidentGroupByDateSeverityScalar(
+                        date=date(int(year), int(month), 1),
+                        severity=severity,
+                        count=value,
+                    )
+                )
+
+    elif group_by == GroupByEnum.DAY:
+        range = period.range("days")
+        for range_date in range:
+            for severity in CalendarIncidentSeverity.values:
+                aggregations[f"{range_date}_{severity}"] = Count(
+                    "id",
+                    filter=Q(
+                        Q(start_datetime__date__lte=range_date)
+                        & Q(
+                            Q(end_datetime__date__gte=range_date)
+                            | Q(end_datetime__isnull=True)
+                        )
+                        & Q(severity=severity)
+                    ),
+                )
+
+        for key, value in (await qs.aaggregate(**aggregations)).items():
+            if value > 0:
+                incident_date, severity = key.split("_")
+                year, month, day = incident_date.split("-")
+
+                return_list.append(
+                    CalendarIncidentGroupByDateSeverityScalar(
+                        date=date(int(year), int(month), int(day)),
+                        severity=severity,
+                        count=value,
+                    )
+                )
+    else:
+        raise NotImplementedError(
+            f"Expected one of 'DAY' or 'MONTH' but got {group_by}"
+        )
 
     return return_list
