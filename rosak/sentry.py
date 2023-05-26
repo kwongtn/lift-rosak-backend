91f69926362406a332a113008e20d3023720024d
Kwong Tung Nan <tungnan5636@gmail.com>
2023-05-27 00:57:11 +0800

    fix(sentry): Capturing transactions for health-check
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- rosak/sentry.py
+++ rosak/sentry.py
@@ -5,7 +5,7 @@ def filter_transactions(event, hint):
     url_string = event["request"]["url"]
     parsed_url = urlparse(url_string)
 
-    if parsed_url.path == "/healthcheck":
+    if parsed_url.path in ["/health-check", "/health-check/"]:
         return None
 
     return event
