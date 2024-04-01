1c349831b5101bea2de48dc7108b11f7fcdab184
Kwong Tung Nan <tungnan5636@gmail.com>
2024-04-01 19:57:22 +0800

    fix(tg): Incorrect spotting date filter target
    Signed-off-by: Kwong Tung Nan <tungnan5636@gmail.com>

--- telegram_provider/utils.py
+++ telegram_provider/utils.py
@@ -23,7 +23,7 @@ async def infinite_retry_on_error(fn: "Coroutine", *args, **kwargs) -> "Coroutin
 
 def get_daily_updates(line_id: int) -> str:
     spotted_today_vehicle_ids = (
-        Event.objects.filter(created__date=date.today())
+        Event.objects.filter(spotting_date=date.today())
         .distinct("vehicle")
         .values_list("vehicle_id", flat=True)
     )
